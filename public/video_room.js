8d297111e1528713374b492c9ae6bc51ecf82af6
kartickg33 <68003347+kartickg33@users.noreply.github.com>
2021-06-26 16:33:33 +0530

    Initial deploy config

--- public/video_room.js
+++ public/video_room.js
@@ -1,5 +1,7 @@
 const io = require("socket.io-client");
-const socket = io('ws://https://peer-connect.herokuapp.com') //server set up
+const socket = io('https://peer-connect.herokuapp.com',{
+  transports:["polling"]
+}) //server set up
 /*
 {transports: ['websocket'], upgrade: false}
 */
