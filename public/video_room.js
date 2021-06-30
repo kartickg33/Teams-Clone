e10074d2c8a4523124373c14c69284bfee741f57
kartickg33 <68003347+kartickg33@users.noreply.github.com>
2021-06-30 23:50:36 +0530

    Updated styling

--- public/video_room.js
+++ public/video_room.js
@@ -92,15 +92,13 @@ function addVideoStream(video, stream) {//stream==video call | add our video on 
 function toggleVideo(stream) {
   stream.getVideoTracks()[0].enabled = !stream.getVideoTracks()[0].enabled ;
   if(!stream.getVideoTracks()[0].enabled){
-    video_change.innerHTML = `<i class="bi bi-camera-video-off" id="icon"></i>`;
-    document.querySelector('.bi-camera-video-off').style.color = "white";
+    video_change.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="36px" viewBox="0 0 24 24" width="36px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M9.56 8l-2-2-4.15-4.14L2 3.27 4.73 6H4c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h12c.21 0 .39-.08.55-.18L19.73 21l1.41-1.41-8.86-8.86L9.56 8zM5 16V8h1.73l8 8H5zm10-8v2.61l6 6V6.5l-4 4V7c0-.55-.45-1-1-1h-5.61l2 2H15z"/></svg>`;
     stop_video.style.backgroundColor = "red";
-    document.querySelector('.bi-camera-video-off').style.transition = "0.2s ease-in";
+    document.querySelector('svg').style.transition = "0.2s ease-in";
     stop_video.style.transition = "0.2s ease-in";
   }
   else{
-    video_change.innerHTML = `<i class="bi bi-camera-video" id="icon"></i>`
-    document.querySelector('.bi-camera-video').style.color = "white";
+    video_change.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="36px" viewBox="0 0 24 24" width="36px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15 8v8H5V8h10m1-2H4c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h12c.55 0 1-.45 1-1v-3.5l4 4v-11l-4 4V7c0-.55-.45-1-1-1z"/></svg>`;
     stop_video.style.backgroundColor = "#147502";
     document.querySelector('.bi-camera-video').style.transition = "0.2s ease-in";
     stop_video.style.transition = "0.2s ease-in";
@@ -112,20 +110,16 @@ function toggleMic(stream) {
 
   stream.getAudioTracks()[0].enabled = !stream.getAudioTracks()[0].enabled;
   if(stream.getAudioTracks()[0].enabled==false){
-    mic_change.innerHTML = `<i class="bi bi-mic-mute" id="icon"></i>`
-    document.querySelector('.bi-mic-mute').style.color = "white";
+    mic_change.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="36px" viewBox="0 0 24 24" width="36px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M10.8 4.9c0-.66.54-1.2 1.2-1.2s1.2.54 1.2 1.2l-.01 3.91L15 10.6V5c0-1.66-1.34-3-3-3-1.54 0-2.79 1.16-2.96 2.65l1.76 1.76V4.9zM19 11h-1.7c0 .58-.1 1.13-.27 1.64l1.27 1.27c.44-.88.7-1.87.7-2.91zM4.41 2.86L3 4.27l6 6V11c0 1.66 1.34 3 3 3 .23 0 .44-.03.65-.08l1.66 1.66c-.71.33-1.5.52-2.31.52-2.76 0-5.3-2.1-5.3-5.1H5c0 3.41 2.72 6.23 6 6.72V21h2v-3.28c.91-.13 1.77-.45 2.55-.9l4.2 4.2 1.41-1.41L4.41 2.86z"/></svg>`;
     stop_mic.style.backgroundColor = "red";
-    document.querySelector('.bi-mic-mute').style.transition = "0.2s";
-    stop_mic.style.transition = "0.2s";
-    console.log("unmute")
+    document.querySelector('svg').style.transition = "0.2s ease-in";
+    stop_mic.style.transition = "0.2s ease-in";
   }
   else{
-    mic_change.innerHTML = `<i class="bi bi-mic" id="icon"></i>`
-    document.querySelector('.bi-mic').style.color = "white";
+    mic_change.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="36px" viewBox="0 0 24 24" width="36px" fill="#FFFFFF"><g><rect fill="none" height="24" width="24"/><rect fill="none" height="24" width="24"/><rect fill="none" height="24" width="24"/></g><g><g/><g><path d="M12,14c1.66,0,3-1.34,3-3V5c0-1.66-1.34-3-3-3S9,3.34,9,5v6C9,12.66,10.34,14,12,14z"/><path d="M17,11c0,2.76-2.24,5-5,5s-5-2.24-5-5H5c0,3.53,2.61,6.43,6,6.92V21h2v-3.08c3.39-0.49,6-3.39,6-6.92H17z"/></g></g></svg>`;
     stop_mic.style.backgroundColor = "#147502";
-    document.querySelector('.bi-mic').style.transition = "0.2s";
-    stop_mic.style.transition = "0.2s";
-    console.log('mute')
+    document.querySelector('svg').style.transition = "0.2s ease-in";
+    stop_mic.style.transition = "0.2s ease-in";
   }
 }
 
