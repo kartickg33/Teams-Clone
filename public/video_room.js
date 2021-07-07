10590cdb12f3e6d4ea8c7bcea7341f5cc964a64f
kartickg33 <68003347+kartickg33@users.noreply.github.com>
2021-07-08 00:57:40 +0530

    video style fix

--- public/video_room.js
+++ public/video_room.js
@@ -97,7 +97,7 @@ function Start_Stop_Video(stream) {
   else{
     video_change.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="36px" viewBox="0 0 24 24" width="36px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15 8v8H5V8h10m1-2H4c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h12c.55 0 1-.45 1-1v-3.5l4 4v-11l-4 4V7c0-.55-.45-1-1-1z"/></svg>`;
     stop_video.style.backgroundColor = "#147502";
-    document.querySelector('.bi-camera-video').style.transition = "0.2s ease-in";
+    document.querySelector('svg').style.transition = "0.2s ease-in";
     stop_video.style.transition = "0.2s ease-in";
   }
 
